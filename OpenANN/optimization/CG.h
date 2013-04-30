3e714d76f5e7d970fbdd2849ce70811404e0c8d7
Alexander Fabisch <afabisch@googlemail.com>
2013-04-30 08:41:03 +0200

    Add line search.

--- OpenANN/optimization/CG.h
+++ OpenANN/optimization/CG.h
@@ -4,6 +4,7 @@
 #include <OpenANN/optimization/Optimizer.h>
 #include <OpenANN/optimization/StoppingCriteria.h>
 #include <OpenANN/io/Logger.h>
+#include <OpenANN/util/EigenWrapper.h>
 #include <Eigen/Dense>
 
 namespace OpenANN {
@@ -41,11 +42,15 @@ class CG : public Optimizer
    * the initial point in the linesearch).
    */
   const double RHO;
+  /**
+   * Maximum of function evaluations per line search.
+   */
+  const int MAX_LINE_SEARCH;
   int lineSearchFailed;
 public:
   CG()
     : debugLogger(Logger::CONSOLE), opt(0), iteration(-1), n(0),
-      SIG(0.1), RHO(0.5*SIG), lineSearchFailed(0)
+      SIG(0.1), RHO(0.5*SIG), MAX_LINE_SEARCH(20), lineSearchFailed(0)
   {
   }
 
@@ -76,13 +81,68 @@ public:
     double reduction = 1.0; // reduction in function value to be expected in the first line-search, TODO
     // 1) Compute error / gradient
     // TODO mini-batch cg
-    error = opt->error();
-    gradient = opt->gradient();
+    Eigen::VectorXd parameters = opt->currentParameters();
+    this->parameters = parameters;
+    double error = opt->error();
+    this->error = error;
+    Eigen::VectorXd gradient = opt->gradient();
+    this->gradient = gradient;
 
     double slope = - gradient.transpose() * gradient;
+    double x1, error1, slope1;
+    double x2, error2, slope2;
     // initial step is red/(|gradient|+1)
-    double x3 = reduction / (1-slope);
+    double x3 = reduction / (1-slope), error3, slope3;
+    Eigen::VectorXd gradient3(n);
+    double x0 = 0, error0 = error, slope0 = slope;
 
+    while(true) // keep extrapolating as long as necessary
+    {
+      x2 = 0.0;
+      error2 = error0;
+      slope2 = slope0;
+      error3 = error0;
+      slope3 = slope0;
+
+      int m = MAX_LINE_SEARCH;
+      bool success = false;
+      double error3;
+      Eigen::VectorXd gradient3(n);
+      do {
+        m--;
+        opt->setParameters(parameters - x3*gradient);
+        error3 = opt->error();
+        gradient3 = opt->gradient();
+        if(isnan(error3) || isinf(error3) || isMatrixBroken(gradient3))
+          x3 = (x2+x3)/2; // bisect and try again
+        else
+          success = true;
+      } while(!success && m > 0);
+
+      // keep best values
+      if(error3 < error)
+      {
+        parameters = parameters - x3*gradient;
+        error = error3;
+        gradient = gradient3;
+      }
+      slope3 = gradient3.transpose() * (-this->gradient); // new slope
+
+      // are we done extrapolating?
+      if(slope3 > SIG*slope || error3 > this->error+x3*RHO*slope)
+        break;
+    }
+
+    // move point 2 to point 1
+    x1 = x2;
+    error1 = error2;
+    gradient1 = gradient2;
+    // move point 3 to point 2
+    x2 = x3;
+    error2 = error3;
+    gradient2 = gradient3;
+    // make cubic extrapolation
+    double a = 6*(error1-error2)+3*(gradient2+gradient1)*(x2-x1);
     // TODO implement
     iteration++;
     bool run = (stop.maximalIterations != stop.defaultValue.maximalIterations
@@ -103,9 +163,10 @@ public:
 private:
   void initialize()
   {
-    parameters.resize(opt->dimension());
+    n = opt->dimension();
+    parameters.resize(n);
     parameters = opt->currentParameters();
-    gradient.resize(opt->dimension());
+    gradient.resize(n);
     gradient.fill(0.0);
     iteration = 0;
   }
